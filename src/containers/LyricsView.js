29f059b8c9f077ae134137cd2f67d6ba5c117422
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-21 13:36:43 -0700

    bring in sing title to lyrics container

--- src/containers/LyricsView.js
+++ src/containers/LyricsView.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import SongTitle from '../components/SongTitle';
+
+const title = 'Best Song Ever';
+const lyrics = 'here are some song lyrics'
+
 
 export default class LyricsView extends Component{
+  
   render(){
     return(
-      <h1>Lyrics View!!! Oh Yeah!!!</h1>
+      <>
+        <h1>Lyrics View!!! Oh Yeah!!!</h1>
+        <SongTitle title={title} lyrics={lyrics} />
+      </>
     );
   }
 }
