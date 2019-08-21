8e99352901b76ab0bbffca4d7a61ec802ff28858
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-08-21 14:14:25 -0700

    musicApi

---
+++ src/services/musicApi.js
@@ -0,0 +1,73 @@
+const corsURL = 'https://cors-anywhere.herokuapp.com/';
+
+const musicApi = {
+  findArtists(search){
+    return fetch(`${corsURL}http://musicbrainz.org/ws/2/artist?query=${search}&fmt=json&limit=25`, {
+      headers: {
+        origins: null
+      }
+    })
+      .then(res => {
+        if(!res.ok) throw 'No artists found';
+        return res.json();
+      })
+      .then(json => ({
+        artists: json.artists
+      }));
+  },
+  getArtistReleases(artistId){
+    return fetch(`${corsURL}http://musicbrainz.org/ws/2/release?artist=${artistId}&fmt=json`, {
+      headers: {
+        origins: null
+      }
+    })
+      .then(res => {
+        if(!res.ok) throw 'No releases found';
+        return res.json();
+      })
+      .then(json => ({
+        releases: json.releases
+      }));
+  },
+  getReleaseCover(releaseId){
+    return fetch(`${corsURL}http://coverartarchive.org/release/${releaseId}/front`, {
+      headers: {
+        origins: null
+      }
+    })
+      .then(res => {
+        if(!res.ok) throw 'No cover art found';
+        return res.json();
+      });
+  },
+  getReleaseRecordings(releaseId){
+    return fetch(`${corsURL}http://musicbrainz.org/ws/2/recording?release=${releaseId}&fmt=json`, {
+      headers: {
+        origins: null
+      }
+    })
+      .then(res => {
+        if(!res.ok) throw 'No recordings found';
+        return res.json();
+      })
+      .then(json => ({
+        recordings: json.recordings
+      }));
+  },
+  getRecordingLyrics(artist, title){
+    return fetch(`${corsURL}https://api.lyrics.ovh/v1/${artist}/${title}`, {
+      headers: {
+        origins: null
+      }
+    })
+      .then(res => {
+        if(!res.ok) throw 'The Lyrics are not to be found!!';
+        return res.json();
+      })
+      .then(json => ({
+        lyrics: json.lyrics
+      }));
+  }
+};
+
+export default musicApi;
