dcbc69b90334c9741cb4aa8be60051dde5bb0abc
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-08-22 14:24:59 -0700

    Got artist id from URL

--- src/containers/ArtistView.js
+++ src/containers/ArtistView.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class ArtistView extends Component{
+  static propTypes = {
+    match: PropTypes.object.isRequired
+  }
+
   render(){
     return(
-      <h1>Artist View!!! YEeehawww!!!</h1>
+      <>
+        <h1>Artist View!!! YEeehawww!!!</h1>
+        <h2>{this.props.match.params.id}</h2>
+      </>
     );
   }
 }
+
+// this.props.match.params.id
