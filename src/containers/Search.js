37ddf9fcce9cdc9969cad6fc6e395143d5eb1ab7
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-08-21 17:39:51 -0700

    Search container started functionality

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
-import musicApi from '../services/musicApi';
+// import musicApi from '../services/musicApi';
+import SearchForm from '../components/search/SearchForm';
+
+const paramsString = window.location.href;
+const searchParams = new URLSearchParams(paramsString);
 
 export default class Search extends Component{
+  state = {
+    search: '',
+    submittedSearch: ''
+  }
   
   // example how to get data
   componentDidMount(){
-    musicApi.findArtists('Prince').then(artists => {
-      console.log(artists);
-    });
+    // musicApi.findArtists('Prince').then(artists => {
+    //   console.log(artists);
+    // });
+  }
+
+  handleInputChange = ({ target }) => {
+    this.setState({ search: target.value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    searchParams.set('search', this.state.search);
+    console.log(paramsString);
+    console.log(searchParams.toString());
+    // this.setState(state => {
+    //   return{ submittedSearch: state.search };
+    // });
   }
 
   render(){
+    const {
+      search
+    } = this.state;
+
     return(
       <>
         <h1>Search!!! Find  it!!</h1>
+        <SearchForm search={search} handleInputChange={this.handleInputChange} handleSubmit={this.handleSubmit} />
         <p>hello</p>
       </>
     );
