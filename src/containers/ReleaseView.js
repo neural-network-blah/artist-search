6edc59b41bbd0af132cacfa70affab94edb12337
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-21 17:36:19 -0700

    ReleaseView update

--- src/containers/ReleaseView.js
+++ src/containers/ReleaseView.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import Poster from '../components/poster/Poster';
+import Title from '../components/title/Title';
+
+const url = 'http://ia800706.us.archive.org/6/items/mbid-032c0dcf-32fb-48df-854c-c4ffdea82009/mbid-032c0dcf-32fb-48df-854c-c4ffdea82009-4732102938.jpg';
 
 export default class ReleaseView extends Component{
   render(){
     return(
-      <h1>Release View!!! Yes sirrreeeee!!!</h1>
+      <>
+        <Title name="stuff" />
+        <Poster url={url} />
+      </>
     );
   }
 }
