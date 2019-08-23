5601a1c1e684005d9f57380ebba6b7e002e5ea69
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-08-23 15:16:43 -0700

    Pager component is paging

--- src/components/pager/Pager.js
+++ src/components/pager/Pager.js
@@ -3,30 +3,37 @@ import PropTypes from 'prop-types';
 // import Link from 'react-router-dom';
 
 function Pager({ pageCount, currentPage, pageClicker }){
+  let disabled1 = false;
+  let disabled2 = false;
+  if(currentPage === 1){
+    disabled1 = true;
+  }
+  if(currentPage === pageCount || pageCount <= 1){
+    disabled2 = true;
+  }
 
-  const arr = [...Array(pageCount).keys()];
+  // const arr = [...Array(pageCount).keys()];
+  // const pages = arr.map(page => {
+  //   if(page === currentPage){
+  //     return(
+  //       <div key={page} >
+  //         <h2 style={{ 'fontStyle': 'italic' }} >{page}</h2>
+  //       </div>
+  //     );
+  //   }
+  //   return(
+  //     <div key={page}>
+  //       <h2>{page}</h2>
+  //     </div>
+  //   );
 
-  const pages = arr.map(page => {
-    if(page === currentPage){
-      return(
-        <div key={page} >
-          <h2 style={{ 'fontStyle': 'italic' }} >{page}</h2>
-        </div>
-      );
-    }
-    return(
-      <div key={page}>
-        <h2>{page}</h2>
-      </div>
-    );
-
-  });
+  // });
   
   return(
     <section>
-      <button name='Previous' onClick={pageClicker(-1)}>👈</button>
-      {pages}
-      <button name='Next' onClick={pageClicker(1)}>👉</button>
+      <button name='Previous' onClick={() => pageClicker(-1)} disabled={disabled1}>👈</button>
+      {/* {pages} */}
+      <button name='Next' onClick={() => pageClicker(1)} disabled={disabled2}>👉</button>
     </section>
   );
 }
