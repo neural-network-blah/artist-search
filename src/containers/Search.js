75fd6b4bcdd6d8ed5447407d7df62112828356fb
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-08-22 18:49:16 -0700

    Paging functionality a work in progress for Search container

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import musicApi from '../services/musicApi';
 import SearchForm from '../components/search/SearchForm';
 import List from '../components/List/List';
-
-const paramsString = window.location.href;
-const searchParams = new URLSearchParams(paramsString);
+import Pager from '../components/pager/Pager';
 
 export default class Search extends Component{
+  static propTypes = {
+    location: PropTypes.object,
+    history: PropTypes.object
+  }
+
   state = {
     search: '',
+    page: 1,
+    pageCount: null,
     arrResult: [],
-    submittedSearch: ''
+    searchParams: {}
   }
   
-  // example how to get data
   componentDidMount(){
-    
+    this.searchParams = new URLSearchParams(this.props.location.search);
+    this.setState({
+      search: this.searchParams.get('search'),
+      page: parseInt(this.searchParams.get('page'))
+    }, () => {
+      musicApi.findArtists(this.state.search, this.state.page).then(artists => {
+        this.setState({ arrResult: artists.artists, pageCount: Math.ceil(artists.count / 25) });
+      });
+    });
   }
 
   handleInputChange = ({ target }) => {
@@ -24,24 +37,26 @@ export default class Search extends Component{
 
   handleSubmit = (event) => {
     event.preventDefault();
-    searchParams.set('search', this.state.search);
-    console.log(paramsString);
-    console.log(searchParams.toString());
+    this.searchParams.set('search', this.state.search);
+    this.searchParams.set('page', this.state.page);
+    this.props.history.push(`/?search=${this.state.search}&page=${this.state.page}`);
+    console.log(this.state.page);
 
-    musicApi.findArtists(this.state.search).then(artists => {
-      console.log(artists);
-      this.setState({ arrResult: artists });
+    musicApi.findArtists(this.state.search, this.state.page).then(artists => {
+      this.setState({ arrResult: artists.artists });
     });
+  }
 
-    // this.setState(state => {
-    //   return{ submittedSearch: state.search };
-    // });
+  pageClicker = (pager) => {
+    this.setState(state => {
+      return{ page: state.page + pager };
+    });
   }
 
   render(){
 
     const {
-      search, arrResult
+      search, arrResult, page
     } = this.state;
 
     return(
@@ -49,7 +64,10 @@ export default class Search extends Component{
         <h1>Search!!! Find  it!!</h1>
         <SearchForm search={search} handleInputChange={this.handleInputChange} handleSubmit={this.handleSubmit} />
         <List array={ arrResult } keyName='id' name='name' location='/artist/' />
+        <Pager pageCount={13} currentPage={page} pageClicker={this.pageClicker} />
       </>
     );
   }
 }
+
+// pageCount, currentPage, clickHandler
